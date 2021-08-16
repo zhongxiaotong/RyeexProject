63b930cf8884c4c8b6ee346dec8486014eb292ab
Greeyzyang <781562942@qq.com>
2021-08-16 14:17:02 +0800

    增加cicd流程中继器

---
+++ ApiTest/Repeater.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time : 2021/8/16 11:53
+# @Author : Greey
+# @FileName: Repeater.py
+
+import os
+import argparse
+import time
+import subprocess
+import multiprocessing
+
+class Repater(object):
+    def __init__(self):
+        current_path = os.path.abspath(__file__)
+        self.father_path = os.path.abspath(os.path.dirname(current_path) + os.path.sep)
+
+    def repeater(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--taskname", type=str, help=u"固件路径", default='baileys')
+        args = parser.parse_args()
+        taskname = args.taskname
+        os.popen("start python " + self.father_path + "\\Run.py --taskname " + taskname)
+        os.popen('pause')
+
+if __name__ == '__main__':
+    t = multiprocessing.Process(target=Repater().repeater)
+    t.start()
