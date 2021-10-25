e3540c96bec00cb257868dddfe1e708b91d8fc43
zhongxiaotong <673421543@qq.com>
2021-10-25 10:34:50 +0800

    更新了一些日志

--- ApiTest/Common/File.py
+++ ApiTest/Common/File.py
@@ -17,33 +17,33 @@ class File(object):
     def __init__(self, zip_src='D:\log'):
         self.zip_src = zip_src
         self.filespath = 'C:\\runner'
-        self.unzip_src = self.zip_src + '\\' + os.path.splitext(self.get_pathfiles())[0]
+        self.unzip_src = self.zip_src + '\\' + os.path.splitext(self.get_pathfiles())[0]        # os.path.splitext(self.get_pathfiles())[0] 这里是要去掉压缩包的后缀名
 
 
     def get_pathfiles(self):
-        filenames = os.listdir(self.zip_src)    #
+        filenames = os.listdir(self.zip_src)    #获取这个压缩包里面的所有文件
 
         if filenames:
             for filename in filenames:
                 if '.zip' in filename:
-                    return filename
+                    return filename             #从传入的压缩包里面查找，在压缩包里面再找到一个压缩包，然后返回这个压缩包
 
     def unzip_file(self):
-        srcfile = self.zip_src + '\\' + self.get_pathfiles()
+        srcfile = self.zip_src + '\\' + self.get_pathfiles()    #压缩包里面的
         list = []
-        r = zipfile.is_zipfile(srcfile)
+        r = zipfile.is_zipfile(srcfile)                 #判断这个文件是一个压缩包
         try:
             if r:
-                fz = zipfile.ZipFile(srcfile, 'r')
-                for file in fz.namelist():
-                    fils = fz.extract(file, self.unzip_src)
-                    list.append(fils)
+                fz = zipfile.ZipFile(srcfile, 'r')      #如果是一个压缩包，那么就创建一个ZipFile（）类对象，操作模式是r(读取)
+                for file in fz.namelist():              #fz.namelist()返回这个压缩包里面的所有文件
+                    fils = fz.extract(file, self.unzip_src)     #将这个压缩包解压到指定的文件路径上面
+                    list.append(fils)           #用列表把这些包全部装起来
             return list
         except:
             raise
 
-    def get_mcuversion(self):
-        configfile = self.unzip_src + "\\config.json"
+    def get_mcuversion(self):               #获取固件版本
+        configfile = self.unzip_src + "\\config.json"       #根据路径找到config.json文件
         try:
             if configfile:
                 with open(configfile, 'r') as f:
